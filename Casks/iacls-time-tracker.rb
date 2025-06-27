8803e2ae13bbd0b3c99c20ac1ed7991eb561453b
markwbennett <mb@ivi3.com>
2025-06-27 16:15:38 -0500

    Update cask URL to latest commit hash

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -2,7 +2,7 @@ cask "iacls-time-tracker" do
   version "1.1.0"
   sha256 "03a88df4006c39a73590c28a04bbf14d86ea2750a8b3ede1636c189b6b5deb6b"
 
-  url "https://github.com/markwbennett/TimeTrackButton/raw/ab040ce8b1bf0ba624537365d7c1c4fa012aa369/TimeTracker_CPP.app.tar.gz"
+  url "https://github.com/markwbennett/TimeTrackButton/raw/45f062c10c6a103026054cbd5ed75eecfd76e4bf/TimeTracker_CPP.app.tar.gz"
   name "IACLS Time Tracker"
   desc "Time tracking application for legal and professional work"
   homepage "https://github.com/markwbennett/TimeTrackButton"
