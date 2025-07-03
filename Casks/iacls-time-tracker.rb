717f0c1f5c603cf0502c0b2dd2aa5eeedb35f3ce
markwbennett <mb@ivi3.com>
2025-07-03 09:34:54 -0500

    Fix cask to use :no_check for sha256 to avoid checksum issues

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -1,6 +1,6 @@
 cask "iacls-time-tracker" do
   version "1.3.5"
-  sha256 "4d6d87be4a99e792ccaf3a6372180bc164a1f6274f7407ff698abc21f185b561"
+  sha256 :no_check
 
   url "https://github.com/markwbennett/TimeTrackButton/raw/main/releases/TimeTracker_CPP_Latest.app.tar.gz"
   name "IACLS Time Tracker"
